d8451a6ed41d706f003e058ae3c5cc25dbb781b8
Rahmurata12 <26190277+Rahmurata12@users.noreply.github.com>
2022-04-19 00:58:41 -0300

    Add files via upload

---
+++ checkpoint2/Aluno.java
@@ -0,0 +1,41 @@
+package checkpoint2;
+
+public class Aluno {
+	
+	private String nome;
+	private String DtNasc;
+	private String RG;
+	private String CPF;
+	private String Tel;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDtNasc() {
+		return DtNasc;
+	}
+	public void setDtNasc(String dtNasci) {
+		this.DtNasc = dtNasci;
+	}
+	public String getRG() {
+		return RG;
+	}
+	public void setRG(String rG) {
+		RG = rG;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public String getTel() {
+		return Tel;
+	}
+	public void setTel(String tel) {
+		Tel = tel;
+	}
+
+}
--- checkpoint2/CadastroAluno.java
+++ checkpoint2/CadastroAluno.java
@@ -1,24 +1,33 @@
-package checkpoint;
+package checkpoint2;
 
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.text.ParseException;
 
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.MaskFormatter;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class CadastroAluno extends JFrame {
-
+	
+	private MaskFormatter ftmTelefone;
+	private MaskFormatter ftmData;
+	private MaskFormatter ftmRG;
+	private MaskFormatter ftmCPF;
 	private JPanel contentPane;
 	private JTextField nome;
 	private JTextField dtNasc;
@@ -26,11 +35,18 @@ public class CadastroAluno extends JFrame {
 	private JTextField CPF;
 	private JTextField tel;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
-
+		
+		String str = "javax.swing.plaf.nimbus.NimbusLookAndFeel";
+		
+		try {
+			UIManager.setLookAndFeel(str);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e1) {
+			e1.printStackTrace();
+		}
+		
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -43,15 +59,29 @@ public class CadastroAluno extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public CadastroAluno() {
 
+	public CadastroAluno() {
+		
+		try {
+			ftmTelefone = new MaskFormatter("##-##-#####-####");
+			ftmData = new MaskFormatter("##/##/####");
+			ftmRG = new MaskFormatter("##.###.###-##");
+			ftmCPF = new MaskFormatter("###.###.###-##");
+		} catch (ParseException e2) {
+			e2.printStackTrace();
+		}
+       
+        
+        ftmTelefone.setValidCharacters("0123456789");
+        ftmData.setValidCharacters("0123456789");
+   
+        ftmTelefone.setValidCharacters("0123456789");
+        ftmData.setValidCharacters("0123456789");
+        
 		Aluno Aluno = new Aluno();
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 253);
+		setBounds(100, 100, 450, 291);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -62,44 +92,44 @@ public class CadastroAluno extends JFrame {
 		contentPane.add(lblNewLabel);
 
 		JLabel lblDataDeNascimento = new JLabel("Data de Nascimento: ");
-		lblDataDeNascimento.setBounds(43, 72, 129, 14);
+		lblDataDeNascimento.setBounds(43, 85, 129, 14);
 		contentPane.add(lblDataDeNascimento);
 
 		JLabel lblRg = new JLabel("RG: ");
-		lblRg.setBounds(43, 106, 32, 14);
+		lblRg.setBounds(43, 125, 32, 14);
 		contentPane.add(lblRg);
 
 		JLabel lblCpf = new JLabel("CPF: ");
-		lblCpf.setBounds(43, 143, 46, 14);
+		lblCpf.setBounds(43, 166, 46, 14);
 		contentPane.add(lblCpf);
 
 		JLabel lblTelefone = new JLabel("Telefone: ");
-		lblTelefone.setBounds(43, 180, 61, 14);
+		lblTelefone.setBounds(43, 208, 61, 14);
 		contentPane.add(lblTelefone);
 
 		nome = new JTextField();
-		nome.setBounds(86, 44, 86, 20);
+		nome.setBounds(86, 40, 86, 24);
 		contentPane.add(nome);
 		nome.setColumns(10);
 
-		dtNasc = new JTextField();
+		dtNasc = new JFormattedTextField(ftmData);
 		dtNasc.setColumns(10);
-		dtNasc.setBounds(187, 69, 86, 20);
+		dtNasc.setBounds(188, 78, 86, 28);
 		contentPane.add(dtNasc);
 
-		RG = new JTextField();
+		RG = new JFormattedTextField(ftmRG);
 		RG.setColumns(10);
-		RG.setBounds(86, 103, 86, 20);
+		RG.setBounds(85, 120, 125, 24);
 		contentPane.add(RG);
 
-		CPF = new JTextField();
+		CPF = new JFormattedTextField(ftmCPF);
 		CPF.setColumns(10);
-		CPF.setBounds(86, 140, 86, 20);
+		CPF.setBounds(86, 160, 124, 26);
 		contentPane.add(CPF);
 
-		tel = new JTextField();
+		tel = new JFormattedTextField(ftmTelefone);
 		tel.setColumns(10);
-		tel.setBounds(102, 177, 86, 20);
+		tel.setBounds(103, 202, 107, 26);
 		contentPane.add(tel);
 
 		JButton btnSalvar = new JButton("Salvar");
